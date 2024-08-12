f76ac11311c4443d8d456b3a009f0356646fe55c
MitaliJ101 <110379575+MitaliJ101@users.noreply.github.com>
2024-08-12 19:04:49 +0530

    BorrowServiceimpl/bookquantitydec(db)/finefunctionality/othrs

--- backEnd/discovery_LMS/src/main/java/com/discovery/constants/Constants.java
+++ backEnd/discovery_LMS/src/main/java/com/discovery/constants/Constants.java
@@ -2,4 +2,6 @@ package com.discovery.constants;
 
 public class Constants {
 	public static final double DAILY_FINE_RATE = 5.0; // Fixed daily fine rate
+	public static final long BORROW_PERIOD_DAYS = 0;
+	
 }
