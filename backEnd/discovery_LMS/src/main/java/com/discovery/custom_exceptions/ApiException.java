7b073692c584b6eda208ee9fa959f98653fbac27
MitaliJ101 <110379575+MitaliJ101@users.noreply.github.com>
2024-08-16 00:24:18 +0530

    Autoupdate of quantity on manageBook table

--- backEnd/discovery_LMS/src/main/java/com/discovery/custom_exceptions/ApiException.java
+++ backEnd/discovery_LMS/src/main/java/com/discovery/custom_exceptions/ApiException.java
@@ -1,7 +1,18 @@
 package com.discovery.custom_exceptions;
 
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.discovery.dto.ApiResponse;
+
 public class ApiException extends RuntimeException {
 	public ApiException(String message) {
 		super(message);
 	}
+
+
+
 }
